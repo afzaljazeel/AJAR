63f57ee0b70ae4545695b1e349ba6e32878ef891
afzaljazeel <154486801+afzaljazeel@users.noreply.github.com>
2025-01-13 13:35:30 +0530

    start

---
+++ app/views/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register page</title>
+</head>
+<body>
+    <section class="form-container">
+
+        <form method="post">
+            <h1>
+                Register now
+            </h1>
+            <input type="text" name="name" placeholder="Enter your name" required>
+            <input type="email" name="email" placeholder="Enter your email" required>
+            <input type="password" name="password" placeholder="Enter your password" required>
+            <input type="password" name="cpassword" placeholder="Confirm your password" required>
+            <input type="submit" name="submit-btn" value="Register class="btn">
+            <p>Already have an account?<a href="login.php">Log In</a> </p>
+
+
+        </form>
+
+
+    </section>
+    
+</body>
+</html>
